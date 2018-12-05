d3ba61d5f77e5ecce0d3c433265a2ebc836b867b
Jorge Diaz <jorge-luis.diaz@gmail.com>
2018-12-04 23:13:24 -0300

    Feat(auth): Crea auth e importa libreria de bootstrap-vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,13 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
