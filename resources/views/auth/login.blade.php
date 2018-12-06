f1a942446d871e563608ccd7d20d239a161fe572
Jorge Diaz <jorge-luis.diaz@gmail.com>
2018-12-05 23:24:52 -0300

    Feat(login): Agrega registro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,35 +12,19 @@
                     
                     <b-form-group
                             label="Correo electronico:"
-                            label-for="email"
-                            description="Nunca compartiremos tu correo con nadie.">
+                            label-for="email">
                         <b-form-input type="email" 
                             id="email" name="email"
                             value="{{ old('email') }}" required autofocus
-                            placeholder="Ingrese aqui tu correo">
+                            placeholder="example@test.com">
                         </b-form-input>
                     </b-form-group>
 
                     <b-form-group label="Contraseña:" label-for="password">
                         <b-form-input type="password"
-                            id="password" name="password"
-                            value="{{ old('password') }}" required>
+                            id="password" name="password" required>
                         </b-form-input>
                     </b-form-group>
-
-                    <!--
-                    @if ($errors->has('email'))
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
-                    @endif
-
-                    @if ($errors->has('password'))
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
-                -->
 
                     <b-form-group>
                         <b-form-checkbox name="remember" id="remember"
@@ -57,13 +41,6 @@
                         ¿Olvidaste tu contraseña?
                     </b-button>
 
-                    <!--
-                    @if (Route::has('password.request'))
-                        <a class="btn btn-link" href=>
-                            {{ __('Forgot Your Password?') }}
-                        </a>
-                    @endif
-                    -->
                 </b-form>
 
             </b-card>
